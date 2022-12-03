5f4292460e47a5233fa6cba6926ff2651f6f6fd8
Call Vin <kanggara75@gmail.com>
2022-12-03 11:03:11 +0700

    add: breeze

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
 		@if (Route::has('login'))
 			<div class="fixed top-0 right-0 hidden px-6 py-4 sm:block">
 				@auth
-					<a class="text-sm text-gray-700 underline dark:text-gray-500" href="{{ url('/home') }}">Home</a>
+					<a class="text-sm text-gray-700 underline dark:text-gray-500" href="{{ url('/dashboard') }}">Dashboard</a>
 				@else
 					<a class="text-sm text-gray-700 underline dark:text-gray-500" href="{{ route('login') }}">Log in</a>
 					@if (Route::has('register'))
