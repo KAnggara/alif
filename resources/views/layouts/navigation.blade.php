fce4c1f5b87b3ff31fcdbfdb51618d136b00474f
Call Vin <kanggara75@gmail.com>
2022-12-05 11:13:14 +0700

    add: dark mode

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,12 +1,13 @@
-<nav class="border-b border-gray-100 bg-white" x-data="{ open: false }">
+<nav class="border-b border-gray-100 bg-white dark:border-gray-700 dark:bg-gray-800" x-data="{ open: false }">
 	<!-- Primary Navigation Menu -->
 	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 		<div class="flex h-16 justify-between">
 			<div class="flex">
+
 				<!-- Logo -->
 				<div class="flex shrink-0 items-center">
 					<a href="{{ route('dashboard') }}">
-						<x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+						<x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
 					</a>
 				</div>
 
@@ -23,9 +24,8 @@
 				<x-dropdown align="right" width="48">
 					<x-slot name="trigger">
 						<button
-							class="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none">
+							class="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none dark:bg-gray-800 dark:hover:text-gray-300">
 							<div>{{ Auth::user()->name }}</div>
-
 							<div class="ml-1">
 								<svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
 									<path fill-rule="evenodd"
@@ -35,16 +35,13 @@
 							</div>
 						</button>
 					</x-slot>
-
 					<x-slot name="content">
 						<x-dropdown-link :href="route('profile.edit')">
 							{{ __('Profile') }}
 						</x-dropdown-link>
-
 						<!-- Authentication -->
 						<form method="POST" action="{{ route('logout') }}">
 							@csrf
-
 							<x-dropdown-link :href="route('logout')"
 								onclick="event.preventDefault();
                                                 this.closest('form').submit();">
@@ -58,7 +55,7 @@
 			<!-- Hamburger -->
 			<div class="-mr-2 flex items-center sm:hidden">
 				<button
-					class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none"
+					class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none dark:text-gray-500 dark:hover:bg-gray-900 dark:hover:text-gray-400 dark:focus:bg-gray-900 dark:focus:text-gray-400"
 					@click="open = ! open">
 					<svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
 						<path class="inline-flex" :class="{ 'hidden': open, 'inline-flex': !open }" stroke-linecap="round"
@@ -80,12 +77,11 @@
 		</div>
 
 		<!-- Responsive Settings Options -->
-		<div class="border-t border-gray-200 pt-4 pb-1">
+		<div class="border-t border-gray-200 pt-4 pb-1 dark:border-gray-600">
 			<div class="px-4">
-				<div class="text-base font-medium text-gray-800">{{ Auth::user()->name }}</div>
+				<div class="text-base font-medium text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
 				<div class="text-sm font-medium text-gray-500">{{ Auth::user()->email }}</div>
 			</div>
-
 			<div class="mt-3 space-y-1">
 				<x-responsive-nav-link :href="route('profile.edit')">
 					{{ __('Profile') }}
@@ -94,13 +90,13 @@
 				<!-- Authentication -->
 				<form method="POST" action="{{ route('logout') }}">
 					@csrf
-
 					<x-responsive-nav-link :href="route('logout')"
 						onclick="event.preventDefault();
-                                        this.closest('form').submit();">
+            this.closest('form').submit();">
 						{{ __('Log Out') }}
 					</x-responsive-nav-link>
 				</form>
+
 			</div>
 		</div>
 	</div>
